6caa81aff52ff73ca325592105c8480dcc4bc48c
jomalsjohn <jomalsjohn@gmail.com>
2015-05-08 13:51:05 -0500

    CourseProject
    Exploratory Graphs and Plots

---
+++ plot1.R
@@ -0,0 +1,7 @@
+myfile <- file("household_power_consumption.txt")
+powerdata<- read.table(text = grep("^[1,2]/2/2007", readLines(myfile), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
+
+
+
+hist(powerdata$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
+
